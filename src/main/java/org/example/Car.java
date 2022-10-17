9b92d57a0518f0db8eb6a0bc41053efc3d9e3e7a
Lyubanya <hamster505@yandex.ru>
2022-10-17 23:37:04 +0300

    add variable

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -2,5 +2,5 @@ package org.example;
 
 public class Car {
     private static final String CAR_NAME = "Volkswagen";
-    private static final String CAR_colour = "black";
+    private static final String CAR_colour = "white";
 }
